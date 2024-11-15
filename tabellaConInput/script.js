08f06e6297867dda66b63ffeb6c8ad6fc7e6f6c4
mazxmirvti <156841523+mazxmirvti@users.noreply.github.com>
2024-11-15 02:32:22 -0500

    Update script.js

--- tabellaConInput/script.js
+++ tabellaConInput/script.js
@@ -1,11 +1,13 @@
 
 function stampa() {
+    const nuovaRiga = [];
     var print=document.getElementById("Nome").value
     var print2=document.getElementById("Cognome").value  
     var print3=document.getElementById("Indirizzo").value  
     var print4=document.getElementById("Città").value  
     var print5=document.getElementById("Email").value
-
+    nuovaRiga.push(print, print2, print3, print4, print5)
+    
     var table = document.getElementById("tabella");
     var row = table.insertRow(-1);
     var cell1 = row.insertCell(0);
@@ -13,9 +15,9 @@ function stampa() {
     var cell3 = row.insertCell(2);
     var cell4 = row.insertCell(3);
     var cell5 = row.insertCell(4);
-    cell1.innerHTML = print;
-    cell2.innerHTML = print2;
-    cell3.innerHTML = print3;
-    cell4.innerHTML = print4;
-    cell5.innerHTML = print5;
+    cell1.innerHTML = nuovaRiga[0];
+    cell2.innerHTML = nuovaRiga[1];
+    cell3.innerHTML = nuovaRiga[2];
+    cell4.innerHTML = nuovaRiga[3];
+    cell5.innerHTML = nuovaRiga[4];
   }
