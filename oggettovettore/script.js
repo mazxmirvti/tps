65e291265aa50935f67475222fd6f9dd3852c1e3
mazxmirvti <156841523+mazxmirvti@users.noreply.github.com>
2025-02-21 16:54:05 +0930

    Update script.js

--- oggettovettore/script.js
+++ oggettovettore/script.js
@@ -1,9 +1,9 @@
 let indice = 0;
 let archivio = [];
 
-// Funzione per salvare i dati inseriti nel form
+
 function memorizzaDati() {
-    const record = {
+    const salva = {
         nome: document.getElementById("nome").value,
         cognome: document.getElementById("cognome").value,
         citta: document.getElementById("citta").value,
@@ -11,28 +11,28 @@ function memorizzaDati() {
         email: document.getElementById("mail").value
     };
 
-    // Aggiunge il record all'archivio e lo salva in localStorage
-    archivio.push(record);
+
+    archivio.push(salva);
     localStorage.setItem("archivio", JSON.stringify(archivio));
 
-    // Resetta i campi del form dopo il salvataggio
+
     document.getElementById("nome").value = "";
     document.getElementById("cognome").value = "";
     document.getElementById("citta").value = "";
     document.getElementById("indirizzo").value = "";
     document.getElementById("mail").value = "";
 
-    // Aggiorna la tabella con i dati salvati
+
     aggiornaTabella();
 }
 
-// Funzione per aggiornare la tabella con i dati memorizzati
+
 function aggiornaTabella() {
     let tabellaDati = document.getElementById("tabella");
     tabellaDati.innerHTML = "";
     let datiSalvati = JSON.parse(localStorage.getItem("archivio")) || [];
 
-    // Ciclo per aggiungere ogni record salvato alla tabella
+
     datiSalvati.forEach((utente, index) => {
         let riga = tabellaDati.insertRow();
         riga.insertCell(0).innerHTML = index + 1; // Numero riga
